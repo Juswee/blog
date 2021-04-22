4d18adb720d53c596eff9ea5b72b7a6ab1ff8732
Juswee <prod.juswee@gmail.com>
2021-04-22 23:25:22 +1000

    fix db lib

--- lib/database.py
+++ lib/database.py
@@ -3,9 +3,10 @@ import psycopg2
 class Database:
     def __init__(self, url):
         self.url = url
-        self.con = psycopg2.connect(url, sslmode='require')
+        self.con = psycopg2.connect(url, sslmode='require')+
+        self.cur = self.con.cursor()
         
-        self.con.execute(f'''CREATE TABLE IF NOT EXISTS Feed (
+        self.cur.execute(f'''CREATE TABLE IF NOT EXISTS Feed (
           enum INTEGER PRIMARY KEY,
           id INTEGER,
           title TEXT,
@@ -19,7 +20,7 @@ class Database:
 
     def get(self, table, indicator, expression=True):
         try:
-            return self.con.execute(f'SELECT {indicator} FROM {table} WHERE {expression}').fetchone()
+            return self.cur.execute(f'SELECT {indicator} FROM {table} WHERE {expression}').fetchone()
         except:
             return None
 
@@ -31,19 +32,19 @@ class Database:
 
     def set(self, table, indicator, expression=True):
         try:
-            self.con.execute(f'UPDATE {table} SET {indicator} WHERE {expression}')
+            self.cur.execute(f'UPDATE {table} SET {indicator} WHERE {expression}')
         except:
             return 'table not exists'
         return self.con.commit()
 
     def push(self, table, data_list):
-        self.con.execute(f'INSERT INTO {table} VALUES (?{", ?" * ( len(data_list) - 1 )})', data_list)
+        self.cur.execute(f'INSERT INTO {table} VALUES (?{", ?" * ( len(data_list) - 1 )})', data_list)
         self.con.commit()
 
     def push_many(self, table, data_list):
         if ( type(data_list) == type([]) or type(data_list) == type(()) ) and type(data_list[0]) != type([]) and type(data_list[0]) != type(()):
             data_list = (data_list)
-        self.con.executemany(f'INSERT INTO {table} VALUES (?{", ?" * ( len(data_list) - 1 )})', data_list)
+        self.cur.executemany(f'INSERT INTO {table} VALUES (?{", ?" * ( len(data_list) - 1 )})', data_list)
         self.con.commit()
 
     def close(self):
