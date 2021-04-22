90a8794ae9ae4f2968b2b538451b2783d14e5a7f
Juswee <prod.juswee@gmail.com>
2021-04-22 22:35:00 +1000

    add db url

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import os
 
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = 'postgres://tgaqugindcjkfq:6e3293b085d408e8593ff424b108674a5d4206f3d4fd3a192c0164525fd0e43d@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d6ei6tn8iogst2'
 UPLOAD_IMAGE = '/static/images/uploads/'
 #ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'svg'}
 
